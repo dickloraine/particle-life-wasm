1b93cc1adb70dfdf275633b3914c19e2a784f764
dickloraine <dickloraine@gmx.net>
2025-02-20 20:20:52 +0100

    Updates

--- src/main.ts
+++
@@ -1,8 +0,0 @@
-import init from 'particle-life-wasm';
-import { App } from './app';
-
-const wasmInit = await init();
-const memory = wasmInit.memory;
-const app = new App('#canvas', memory);
-
-app.run();
